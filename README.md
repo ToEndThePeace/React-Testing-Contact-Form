9efbb1f421a2e41d99d65ccf0d3e380a15622ef0
ToEndThePeace <bran.ramirez.don@gmail.com>
2020-05-07 16:37:20 -0700

    Finished

--- README.md
+++ README.md
@@ -29,9 +29,9 @@ As a developer, you will be writing tests for every component. As we've learned,
 - [done] Push commits: git push origin `<firstName-lastName>`.
 
 **Test Runner**
-- [ ] In a new terminal window, run `npm test`
-- [ ] With each saved change in your editor, the test runner will re-run all the tests
-- [ ] **IMPORTANT** If a test fails, use the test runner's error messages to find out why it is failing
+- [done] In a new terminal window, run `npm test`
+- [done] With each saved change in your editor, the test runner will re-run all the tests
+- [done] **IMPORTANT** If a test fails, use the test runner's error messages to find out why it is failing
 
 ## Instructions and/or completion requirements (MVP)
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,32 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-test("renders App without crashing", () => {
-  render(<App />);
+test("App is rendered to the document", () => {
+  const { container } = render(<App />);
+  expect(container).toBeInTheDocument();
+});
+
+test("All expected form inputs are present", () => {
+  const { getByLabelText: get, getByTestId: getId } = render(<App />);
+  expect(get(/first name/i)).toBeInTheDocument();
+  expect(get(/last name/i)).toBeInTheDocument();
+  expect(get(/email/i)).toBeInTheDocument();
+  expect(get(/message/i)).toBeInTheDocument();
+  expect(getId(/submit/i)).toBeInTheDocument();
+});
+
+test("Form can be successfully filled out and submitted", async () => {
+  const { getByLabelText: get, getByTestId: getId, getByText: getTx } = render(
+    <App />
+  );
+  fireEvent.change(get(/first name/i), { target: { value: "Bra" } });
+  fireEvent.change(get(/last name/i), { target: { value: "Ramirez" } });
+  fireEvent.change(get(/email/i), { target: { value: "br@nd.on" } });
+  fireEvent.change(get(/message/i), { target: { value: "message text" } });
+  fireEvent.click(getId("submit"));
+
+  setTimeout(() => {
+    expect(getTx(/ramirez/i)).toBeInTheDocument();
+  });
 });
